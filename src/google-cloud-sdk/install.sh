6b343bc7f482d48f7cec0ae6cd074545ae8fd526
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-08-01 17:29:55 +0200

    Fix gcloud install directory

--- src/google-cloud-sdk/install.sh
+++ src/google-cloud-sdk/install.sh
@@ -2,14 +2,43 @@
 
 set -e
 
+export CLOUDSDK_HOME=${CLOUDSDK_HOME:-"/usr/local/google-cloud-sdk"}
+
 if [ "$(id -u)" -ne 0 ]; then
 	echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
 	exit 1
 fi
 
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+	USERNAME=""
+	POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+	for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+		if id -u "${CURRENT_USER}" >/dev/null 2>&1; then
+			USERNAME="${CURRENT_USER}"
+			break
+		fi
+	done
+	if [ "${USERNAME}" = "" ]; then
+		USERNAME=root
+	fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} >/dev/null 2>&1; then
+	USERNAME=root
+fi
+
 # Run Google Cloud CLI installation script
 curl -fsSL https://sdk.cloud.google.com | bash -s -- \
 	--disable-prompts \
-	--install-dir="/usr/local"
+	--install-dir="${CLOUDSDK_HOME}" \
+
+# Create gcloud group, dir, and set sticky bit
+if ! cat /etc/group | grep -e "^gcloud:" > /dev/null 2>&1; then
+	groupadd -r gcloud
+fi
+usermod -a -G gcloud ${USERNAME}
+umask 0002
+chown -R "${USERNAME}:gcloud" ${CLOUDSDK_HOME}
+chmod -R g+r+w "${CLOUDSDK_HOME}"
+find "${CLOUDSDK_HOME}" -type d -print0 | xargs -0 -n 1 chmod g+s
 
 echo "Done!"
