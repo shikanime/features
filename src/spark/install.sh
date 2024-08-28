8f8d545664df315960d4cf301e27618ec7bde989
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-08-28 16:49:26 +0000

    Fix Spark permissions

--- src/spark/install.sh
+++ src/spark/install.sh
@@ -4,9 +4,30 @@ set -e
 
 VERSION="${VERSION:-"latest"}"
 
+export SPARK_HOME=${SPARK_HOME:-"/usr/local/spark"}
+
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
+
 if [ "$(id -u)" -ne 0 ]; then
 	echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
 	exit 1
+fi
+
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+	USERNAME=""
+	POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+	for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+		if id -u "${CURRENT_USER}" >/dev/null 2>&1; then
+			USERNAME="${CURRENT_USER}"
+			break
+		fi
+	done
+	if [ "${USERNAME}" = "" ]; then
+		USERNAME=root
+	fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} >/dev/null 2>&1; then
+	USERNAME=root
 fi
 
 find_version_list() {
@@ -29,3 +50,13 @@ curl -fsSL \
     https://archive.apache.org/dist/spark/spark-${requested_version}/spark-${requested_version}-bin-hadoop3.tgz \
     | tar xz -C /usr/local/spark
 ln -s /usr/local/spark/spark-${requested_version}-bin-hadoop3 /usr/local/spark/current
+
+# Create spark group, dir, and set sticky bit
+if ! cat /etc/group | grep -e "^spark:" > /dev/null 2>&1; then
+	groupadd -r spark
+fi
+usermod -a -G spark ${USERNAME}
+umask 0002
+chown -R "${USERNAME}:spark" ${SPARK_HOME}
+chmod -R g+r+w "${SPARK_HOME}"
+find "${SPARK_HOME}" -type d -print0 | xargs -0 -n 1 chmod g+s
